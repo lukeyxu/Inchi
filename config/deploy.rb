3b9e48248ed2912d5a1ef2f27eb01f861f78e304
lukeyxu <sdoushi1@gmail.com>
2016-02-01 22:04:10 -0600

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock '3.4.0'
 set :application, 'Inchi'
 set :repo_url, 'git@github.com:lukeyxu/Inchi.git'
 
+set :passenger_restart_with_touch, true
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
