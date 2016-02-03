facc103ef3f11686447a488ae83232ff42e626c3
lukeyxu <sdoushi1@gmail.com>
2016-02-03 14:07:24 -0600

    updated img tag

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,8 @@ set :deploy_to, '/home/deploy/Inchi'
 # Default value for :scm is :git
 # set :scm, :git
 
+load 'deploy/assets'
+
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,12 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: '162.243.30.78', port: 3000 }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
